28081540887c7bd6ece002f2f9d4e353a02d8743
waifushork <waifushork@gmail.com>
2021-09-07 01:48:36 -0400

    removed logger factory from config

--- src/Waifu.NET/WaifuConfiguration.cs
+++ src/Waifu.NET/WaifuConfiguration.cs
@@ -4,7 +4,6 @@
 	
 	public sealed class WaifuConfiguration
 	{
-		public ILoggerFactory LoggerFactory { internal get; set; }
 		public ILogger Logger { internal get; set; }
 		public string[] DefaultExcludes { internal get; set; }
 	}
